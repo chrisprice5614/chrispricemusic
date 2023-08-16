cc71d075de6c9bff0afa8f6cd286bc0660d41759
chrisprice5614 <33729338+chrisprice5614@users.noreply.github.com>
2023-08-16 14:15:43 -0700

    Add files via upload

---
+++ posts.js
@@ -0,0 +1,9 @@
+///id = YYYYMMDDX X = Number of article that day
+//Always add to the top
+var items = [
+    {
+        title: "August 2023",
+        link: "/blog/august2023.html"
+    },
+    
+]
